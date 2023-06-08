6fd985a7408c0ee6e95f3d33ea0e3ef0181ad5ca
GGrinch <giorgi.minashvili.1@btu.edu.ge>
2023-06-08 12:26:55 +0400

    Testing

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index_page():
@@ -16,5 +12,4 @@ def about_page():
     return render_template('about.html')
 
 with app.app_context():
-    db.create_all()
     app.run()
